0fb1ecb60333945314718735a886c2399c8e7f83
ecophilina <paenglis@lakeheadu.ca>
2019-06-11 23:35:22 -0700

    add arguments for: min vector plotted, type of marker for raster cells with no analog, and setting limits for raster colour scale (values beyond coloured red)

--- R/plot.R
+++ R/plot.R
@@ -21,11 +21,11 @@
 #'    If NULL, will attempt to create bathymetry layer for xy values in df using gfplot.
 #' @param arrowhead_size Changes head size for custom geom_quiver function.
 #' @param axis_lables Logical for inclusion of axis labels.
-#' @param viridis_option Change between viridis colormap options available in ggplot. 
-#' @param transform_col Apply transformation to colour scale. 
-#'    Accepts standard options (e.g. "sqrt") or custom transformations without quotes 
-#'    (e.g. fourth_root_power defined internally using scales::trans_new). 
-#'    Default is no transformation (no_trans).
+#' @param viridis_option Change between viridis colormap options available in ggplot.
+#' @param transform_col Apply transformation to colour scale.
+#'    Accepts standard options (e.g. "sqrt") or unquoted custom transformations
+#'    defined using scales::trans_new (e.g. fourth_root_power).
+#'    Default is to apply no transformation (no_trans).
 #'
 #' @export
 #'
@@ -38,6 +38,8 @@ plot_vocc <- function(df,
                       vec_lwd_range = c(0.7, 0.8),
                       vec_alpha = 1,
                       max_vec_plotted = max(df$distance),
+                      min_vec_plotted = 4,
+                      NA_label = "NA",
                       low_fill = "Steel Blue 4",
                       mid_fill = "white",
                       high_fill = "Red 3",
@@ -46,52 +48,69 @@ plot_vocc <- function(df,
                       high_col = "white",
                       coast = NULL,
                       contours = NULL,
+                      # facet = FALSE,
                       axis_lables = FALSE,
                       viridis_option = "D",
-                      transform_col = no_trans
+                      transform_col = no_trans,
+                      raster_limits = NULL
   ) {
   df <- df[order(-df$distance), ] # order so smaller vectors are on top?
-
+  df[df$distance < min_vec_plotted, ]$target_X <- NA
+  df[df$distance < min_vec_plotted, ]$target_Y <- NA
+  
   if (max(df$distance) > max_vec_plotted) {
     df[df$distance > max_vec_plotted, ]$target_X <- NA
     df[df$distance > max_vec_plotted, ]$target_Y <- NA
   }
-  
+
   if (isFALSE(axis_lables)) {
     gvocc <- ggplot2::ggplot(df, aes(x, y)) +
       coord_fixed(xlim = range(df$x) + c(-3, 3), ylim = range(df$y) + c(-3, 3)) +
-      gfplot::theme_pbs() + theme(axis.title.x=element_blank(), axis.title.y=element_blank()) 
+      gfplot::theme_pbs() + theme(axis.title.x = element_blank(), axis.title.y = element_blank())
   } else {
     gvocc <- ggplot2::ggplot(df, aes(x, y)) +
       coord_fixed(xlim = range(df$x) + c(-3, 3), ylim = range(df$y) + c(-3, 3)) +
       gfplot::theme_pbs() + xlab("UTM") + ylab("UTM")
   }
-  
+
 
   #### Add fill ####
   if (!is.null(fill_col)) {
     fill <- df[[fill_col]]
-    breaks <- scales::trans_breaks(transform_col[["transform"]], 
-      transform_col[["inverse"]], n=6)
-    labels <- function(x) { format(x, digits = 1) }
     
+    breaks <- scales::trans_breaks(transform_col[["transform"]],
+      transform_col[["inverse"]],
+      n = 6)
+    
+    
+    labels <- function(x) {
+      format(x, digits = 1, scientific = FALSE)
+    }
+
     if (min(fill, na.rm = TRUE) > 0) {
       gvocc <- gvocc +
         geom_raster(aes(fill = fill), alpha = raster_alpha) +
-        scale_fill_viridis_c(option = viridis_option,
-          trans = transform_col, breaks = breaks, labels = labels) +
+        scale_fill_viridis_c(
+          option = viridis_option, na.value = "red",
+          trans = transform_col, breaks = breaks, labels = labels, limits = raster_limits
+        ) +
         labs(fill = fill_label) +
         theme(legend.position = c(0.17, 0.17))
     } else {
       gvocc <- gvocc +
         geom_raster(aes(fill = fill), alpha = raster_alpha) +
-        scale_fill_gradient2(low = low_fill, mid = mid_fill, high = high_fill, 
-          trans = transform_col, breaks = breaks, labels = labels) +
+        scale_fill_gradient2(
+          low = low_fill, mid = mid_fill, high = high_fill,
+          trans = transform_col, breaks = breaks, labels = labels
+        ) +
         labs(fill = fill_label) +
         theme(legend.position = c(0.17, 0.17))
     }
   }
 
+  # #### Facet by year ####
+  # if (facet) { gvocc <- gvocc + facet_wrap(~year)}
+  #
   #### Add bathymetry ####
   if (!is.null(contours)) {
     # add premade contour layers (will all be same colour and must be in utms)
@@ -206,6 +225,13 @@ plot_vocc <- function(df,
 
   ####  Add arrows indicating target cells ####
   if (!is.null(vec_aes)) {
+    # df <- df[order(-df$distance), ] # order so smaller vectors are on top?
+    #
+    # if (max(df$distance) > max_vec_plotted) {
+    #   df[df$distance > max_vec_plotted, ]$target_X <- NA
+    #   df[df$distance > max_vec_plotted, ]$target_Y <- NA
+    # }
+    #
     vector <- as.vector(na.omit(df[[vec_aes]]))
 
     gvocc <- gvocc +
@@ -220,27 +246,146 @@ plot_vocc <- function(df,
 
 
     # add NAs to plot where grid cells have no target cell within the distance range of 'max_vec_plotted'
+    
     gvocc <- gvocc +
       guides(colour = "none", size = "none") +
       geom_text(
         data = df[df$distance > max_vec_plotted, ], aes(x, y), inherit.aes = FALSE,
         size = 2, colour = "grey99",
-        alpha = 0.75, label = "NA"
+        alpha = 0.75, label = NA_label
       )
   }
-  
+
   gvocc
-  
 }
 
-fourth_root_power <- scales::trans_new(
-    name = "fourth root power",
-    transform = function(x) ifelse(x > 0, x^0.25, -(-x)^0.25),
-    inverse = function(x) ifelse(x > 0, x^4, -(-x)^4),
-    domain = c(Inf, Inf))
 
+#' Raster map with year facets
+#'
+#' @param df Dataframe.
+#' @param column Name column to be plotted.
+#'
+#' @export
+plot_facet_map <- function(df, column = "est",
+                           X = "X", Y = "Y",
+                           viridis_option = "C",
+                           transform_col = no_trans,
+                           raster_limits = NULL
+  ) {
+
+  breaks <- scales::trans_breaks(transform_col[["transform"]],
+    transform_col[["inverse"]],
+    n = 5
+  )
+  labels <- function(x) {
+    format(x, digits = 1)
+  }
+  
+  
+  gfacet <- ggplot(df, aes_string(X, Y, fill = column)) +
+    geom_raster() +
+    scale_fill_viridis_c(
+      option = viridis_option, na.value = "red",
+      trans = transform_col, breaks = breaks, labels = labels, limits = raster_limits
+    ) +
+    facet_wrap(~year) +
+    coord_fixed(xlim = range(df$X) + c(-3, 3), ylim = range(df$Y) + c(-3, 3)) +
+    gfplot::theme_pbs() + theme(axis.title.x = element_blank(), axis.title.y = element_blank())
+  # theme(legend.position = c(0.9, 0),
+  #   legend.justification = c(0.9, 0),
+  #   legend.title = element_blank())
+  #
+  
+  # convert coordinate data to lat lon
+  df <- df %>%
+    dplyr::mutate(X = X, Y = Y) %>%
+    gfplot:::utm2ll(., utm_zone = 9)
+  
+  #### Add bathymetry ####
+  # add bathymetry layer from gfplot
+  # select bathymetry lines (output in utm) for area defined in lat lon
+  isobath <- gfplot:::load_isobath(
+    range(df$X) + c(-5, 5),
+    range(df$Y) + c(-5, 5),
+    bath = c(100, 200, 300, 400, 500),
+    utm_zone = 9
+  )
+  isobath$PID2 <- as.factor(isobath$PID)
+
+  # add lines to plot
+  # TODO: should functionize so that colours change automatically with contour PID
+  gfacet <- gfacet +
+    geom_path(
+      data = isobath[isobath$PID == 100, ],
+      aes_string(
+        x = X, y = Y,
+        group = "paste(PID, SID)"
+      ),
+      inherit.aes = FALSE, lwd = 0.4, alpha = 0.4, colour = "grey85"
+    ) +
+    geom_path(
+      data = isobath[isobath$PID == 200, ],
+      aes_string(
+        x = X, y = Y,
+        group = "paste(PID, SID)"
+      ),
+      inherit.aes = FALSE, lwd = 0.4, alpha = 0.4, colour = "grey70"
+    ) +
+    geom_path(
+      data = isobath[isobath$PID == 300, ],
+      aes_string(
+        x = X, y = Y,
+        group = "paste(PID, SID)"
+      ),
+      inherit.aes = FALSE, lwd = 0.4, alpha = 0.4, colour = "grey55"
+    ) +
+    geom_path(
+      data = isobath[isobath$PID == 400, ],
+      aes_string(
+        x = X, y = Y,
+        group = "paste(PID, SID)"
+      ),
+      inherit.aes = FALSE, lwd = 0.4, alpha = 0.4, colour = "grey40"
+    ) +
+    geom_path(
+      data = isobath[isobath$PID == 500, ],
+      aes_string(
+        x = X, y = Y,
+        group = "paste(PID, SID)"
+      ),
+      inherit.aes = FALSE, lwd = 0.4, alpha = 0.4, colour = "grey30"
+    )
+
+  #### Add coast ####
+  # creates coast lines for area defined in lat lon
+  coast <- gfplot:::load_coastline(
+    range(df$X) + c(-1, 1),
+    range(df$Y) + c(-1, 1),
+    utm_zone = 9
+  )
+
+  # add polygons to plot
+  gfacet <- gfacet +
+    geom_polygon(
+      data = coast, aes_string(x = X, y = Y, group = "PID"),
+      fill = "grey87", col = "grey70", lwd = 0.2
+    )
+  gfacet
+}
+
+
+#' @export
+fourth_root_power <- scales::trans_new(
+  name = "fourth root power",
+  transform = function(x) ifelse(x > 0, x^0.25, -(-x)^0.25),
+  inverse = function(x) ifelse(x > 0, x^4, -(-x)^4),
+  domain = c(Inf, Inf)
+)
+
+#' @export
 no_trans <- scales::trans_new(
-  name = "no transformation",
+  name = "no trans",
   transform = function(x) x,
   inverse = function(x) x,
-  domain = c(Inf, Inf))
+  domain = c(Inf, Inf)
+)
