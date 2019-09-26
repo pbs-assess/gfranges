e62bb97d2debcf2a69c15328fc911e1f3357e67e
ecophilina <paenglis@lakeheadu.ca>
2019-09-26 11:21:05 -0700

    add back fixed sigma E option

---
+++ analysis/VOCC/basic_spatial_re_fix_sigma.cpp
@@ -0,0 +1,207 @@
+#include <TMB.hpp>
+
+template <class Type>
+bool isNA(Type x)
+{
+  return R_IsNA(asDouble(x));
+}
+
+// ------------------ Main TMB template ----------------------------------------
+
+template <class Type>
+Type objective_function<Type>::operator()()
+{
+  using namespace R_inla;
+  using namespace density;
+  using namespace Eigen;
+  
+  // Vectors of real data
+  DATA_VECTOR(y_i);      // response
+  DATA_MATRIX(X_ij);     // model matrix
+  
+  // DATA_INTEGER(n_t);  // number of years
+  
+  // DATA_SPARSE_MATRIX(A); // INLA 'A' projection matrix for original data
+  DATA_SPARSE_MATRIX(A_sk); // INLA 'A' projection matrix for unique stations
+  DATA_IVECTOR(A_spatial_index); // Vector of stations to match up A_sk output
+  
+  // Indices for factors
+  // DATA_FACTOR(year_i);
+  
+  // SPDE objects from R-INLA
+  DATA_STRUCT(spde, spde_t);
+  
+  DATA_IVECTOR(k_i); // species index
+  DATA_INTEGER(n_k);   // number of species
+  DATA_IVECTOR(species_id_k);   // species identifier to calculate the mean for each species across years
+  // DATA_INTEGER(n_just_species);  // the true number of species
+  DATA_VECTOR(n_years_per_species);
+  
+  DATA_VECTOR(intercept_i);
+  DATA_VECTOR(after_i);
+  DATA_VECTOR(source_i);
+  DATA_IVECTOR(m_i); // cell ID
+  // DATA_INTEGER(interaction_position);
+  
+  // ------------------ Parameters ---------------------------------------------
+  
+  // Parameters
+  // Fixed effects
+  PARAMETER_VECTOR(b_j);  // fixed effect parameters
+  PARAMETER_ARRAY(b_re);  // re parameters
+  PARAMETER_VECTOR(b_re_sp);  // re parameters
+  PARAMETER_VECTOR(b_cell);  // re parameters
+  PARAMETER_VECTOR(log_gamma);  // re parameter sigmas
+  PARAMETER(log_omega);  // re sp-level mean BACI interactions
+  // PARAMETER_VECTOR(log_omega);  // re sp-level mean BACI interactions
+  PARAMETER(log_varphi);  // re cell sigma
+  // PARAMETER(ln_tau_O);    // spatial process
+  PARAMETER(ln_tau_E);    // spatio-temporal process
+  PARAMETER(ln_kappa);    // Matern parameter
+  
+  PARAMETER(ln_phi);           // sigma / dispersion / etc.
+  
+  // Random effects
+  // PARAMETER_VECTOR(omega_s);    // spatial effects; n_s length
+  PARAMETER_ARRAY(epsilon_sk);  // spatio-temporal effects; n_s by n_t matrix
+  
+  // ------------------ End of parameters --------------------------------------
+  
+  int n_i = y_i.size();   // number of observations
+ // int n_j = X_ij.cols();  // number of fixed effect parameters
+  
+  Type nll_data = 0;     // likelihood of data
+  Type nll_epsilon = 0;    // spatial effects
+  Type nll_re = 0;  // re
+
+  // ------------------ Geospatial ---------------------------------------------
+  
+  // Matern:
+  Type range = sqrt(Type(8.0)) / exp(ln_kappa);
+  // Type sigma_O = 1 / sqrt(Type(4.0) * M_PI * exp(Type(2.0) * ln_tau_O) *
+  //   exp(Type(2.0) * ln_kappa));
+  // REPORT(sigma_O);
+  // ADREPORT(sigma_O);
+  
+  Type sigma_E = 1 / sqrt(Type(4.0) * M_PI * exp(Type(2.0) * ln_tau_E) *
+    exp(Type(2.0) * ln_kappa));
+  REPORT(sigma_E);
+  ADREPORT(sigma_E);
+  
+  Eigen::SparseMatrix<Type> Q; // Precision matrix
+  
+  Q = R_inla::Q_spde(spde, exp(ln_kappa));
+  
+  // ------------------ INLA projections ---------------------------------------
+  
+  // Here we are projecting the spatial random effects to the
+  // locations of the data using the INLA 'A' matrices.
+  array<Type> epsilon_sk_A(A_sk.rows(), A_sk.cols());
+  for (int k = 0; k < n_k; k++)
+    epsilon_sk_A.col(k) = A_sk * vector<Type>(epsilon_sk.col(k));
+  // vector<Type> omega_s_A = A * omega_s;
+  vector<Type> epsilon_sk_A_vec(n_i);
+  
+  // ------------------ Linear predictor ---------------------------------------
+  
+  vector<Type> eta_fixed_i = X_ij * b_j;
+  vector<Type> mu_i(n_i), eta_i(n_i);
+  for (int i = 0; i < n_i; i++) {
+    eta_i(i) = Type(0);
+  }
+  for (int i = 0; i < n_i; i++) {
+    eta_i(i) = eta_fixed_i(i) + 
+      b_re(k_i(i),0) * intercept_i(i) + 
+      b_re(k_i(i),1) * after_i(i) + 
+      b_re(k_i(i),2) * source_i(i) +
+      b_re(k_i(i),3) * after_i(i) * source_i(i) + // spp./year BACI interaction random effect 
+      b_re_sp(species_id_k(k_i(i))) * after_i(i) * source_i(i) + // spp. level BACI interaction
+      b_cell(m_i(i)) * Type(1.0); // cell match
+    
+    epsilon_sk_A_vec(i) = epsilon_sk_A(A_spatial_index(i), k_i(i)); // record it
+      
+    eta_i(i) += epsilon_sk_A_vec(i);  // spatial
+    mu_i(i) = eta_i(i);
+  }
+  
+  // ------------------ Probability of random effects --------------------------
+  
+  for(int k = 0; k < b_re.rows(); k++) {
+    for(int r = 0; r < (b_re.cols() - 1); r++) {
+      int z;
+      // 1st and 2nd random effects share a variance bc cells are matched
+      if (r == 0) { 
+        z = 0;
+      } else {
+        z = r - 1;
+      }
+      nll_re -= dnorm(b_re(k,r), Type(0.0), exp(log_gamma(z)), true);
+    }
+  }
+  for(int k = 0; k < b_re.rows(); k++) {
+    // nll_re -= dnorm(b_re(k,3), Type(0.0), exp(log_omega(species_id_k(k))), true);
+    nll_re -= dnorm(b_re(k,3), Type(0.0), exp(log_omega), true);
+  }
+  // for(int u = 0; u < n_just_species; u++) {
+  //   nll_re -= dnorm(b_re_sp(u), Type(0.0), exp(log_omega), true);
+  // }
+
+  for(int m = 0; m < b_cell.size(); m++) {
+    nll_re -= dnorm(b_cell(m), Type(0.0), exp(log_varphi), true);
+  }
+  
+  // Spatial random effects:
+  for (int k = 0; k < n_k; k++) {
+    nll_epsilon += SCALE(GMRF(Q, true), 1. / exp(ln_tau_E))(epsilon_sk.col(k));
+  }
+  
+  // ------------------ Probability of data given random effects ---------------
+  
+  for (int i = 0; i < n_i; i++) {
+    if (!isNA(y_i(i))) {
+      nll_data -= dnorm(y_i(i), mu_i(i), exp(ln_phi), true);
+    }
+  }
+  // ------------------ Predictions on new data --------------------------------
+  // ------------------ Derived quantities ---------------------------------
+  
+  vector<Type> b_baci_interaction(n_k);
+  
+  for(int k = 0; k < b_re.rows(); k++) {
+    b_baci_interaction(k) = b_re(k,3) + b_re_sp(species_id_k(k)); // + b_j(interaction_position);
+  }
+  
+  // vector<Type> b_baci_interaction_spp(n_just_species);
+  // for(int k = 0; k < b_re.rows(); k++) {
+  //   b_baci_interaction_spp(species_id_k(k)) += b_baci_interaction(k);
+  // }
+  // for(int i = 0; i < n_years_per_species.size(); i++) {
+  //   b_baci_interaction_spp(i) = b_baci_interaction_spp(i) / n_years_per_species(i);
+  // }
+  // 
+  REPORT(b_baci_interaction);    
+  ADREPORT(b_baci_interaction);
+  
+  // REPORT(b_baci_interaction_spp);    
+  // ADREPORT(b_baci_interaction_spp);
+  
+  // ------------------ Reporting ----------------------------------------------
+  
+  // REPORT(sigma_E);      // spatio-temporal process parameter
+  // ADREPORT(sigma_E);      // spatio-temporal process parameter
+  REPORT(epsilon_sk_A_vec);   // spatio-temporal effects; vector
+  // REPORT(b_rw_t);   // time-varying effects
+  // REPORT(omega_s_A);      // spatial effects; n_s length vector
+  // REPORT(omega_s_trend_A); // spatial trend effects; n_s length vector
+  // REPORT(eta_fixed_i);  // fixed effect predictions in the link space
+  // REPORT(eta_i);        // fixed and random effect predictions in link space
+  // REPORT(eta_rw_i);     // time-varying predictions in link space
+  // REPORT(rho);          // AR1 correlation in -1 to 1 space
+  REPORT(range);        // Matern approximate distance at 10% correlation
+  // ADREPORT(range);      // Matern approximate distance at 10% correlation
+  
+  // ------------------ Joint negative log likelihood --------------------------
+  
+  Type jnll = nll_data + nll_epsilon + nll_re;
+  return jnll;
+}
---
+++ analysis/VOCC/vocc-modelling-fix-sigma.R
@@ -0,0 +1,222 @@
+library(dplyr)
+library(ggplot2)
+
+library(TMB)
+#setwd(here::here())
+#files <- list.files("../rockfish-vocc-temp/perc_50/0.75/", full.names = TRUE)
+
+setwd("analysis/VOCC/")
+
+# files <- list.files("data/_all/temperature/perc_50/0.25/adult/", full.names = TRUE) 
+# files <- list.files("data/_all/temperature/perc_50/0.5/adult/", full.names = TRUE) 
+# # files <- list.files("data/_all/temperature/perc_50/0.25/imm/", full.names = TRUE) 
+# # files <- list.files("data/_all/temperature/perc_50/0.5/imm/", full.names = TRUE) 
+
+files <- list.files("data/_all/temperature/perc_25/0.25/adult/", full.names = TRUE) 
+files <- list.files("data/_all/temperature/perc_25/0.5/adult/", full.names = TRUE) 
+# files <- list.files("data/_all/temperature/perc_25/0.25/imm/", full.names = TRUE) 
+ files <- list.files("data/_all/temperature/perc_25/0.5/imm/", full.names = TRUE) 
+# 
+# # files <- list.files("data/_all/do/perc_50/0.25/adult/", full.names = TRUE) 
+#  files <- list.files("data/_all/do/perc_50/0.5/adult/", full.names = TRUE) 
+# # files <- list.files("data/_all/do/perc_50/0.25/imm/", full.names = TRUE) 
+#  files <- list.files("data/_all/do/perc_50/0.5/imm/", full.names = TRUE) 
+
+# files <- list.files("data/_all/do/perc_25/0.25/adult/", full.names = TRUE)
+# # files <- list.files("data/_all/do/perc_25/0.5/adult/", full.names = TRUE) 
+# files <- list.files("data/_all/do/perc_25/0.25/imm/", full.names = TRUE) # only 3 sp
+# # files <- list.files("data/_all/do/perc_25/0.5/imm/", full.names = TRUE) 
+model_type <- gsub("/", " ", gsub("//vocc..*", " ", gsub("data/_all/", " ", files[1])))
+ 
+.d <- purrr::map_dfr(files, readRDS)
+
+.d <- .d %>% group_by(species) %>% mutate(count = n()) %>% filter(count > 100)
+unique(.d$count)
+
+rm(d)
+d <- select(.d, species, log_density, after, cell_type, log_depth, icell, start_time, X, Y, vect_dist, matchobs)
+d <- mutate(d, source = ifelse(cell_type == "source", 1, 0)) #, age = "mature"
+
+unique(d$start_time)
+
+# d <- filter(d, start_time == "2013")
+# d <- filter(d, start_time == "2015")
+nrow(d)
+
+ggplot(d, aes(X, Y, colour = cell_type)) + geom_point(size = 0.1, alpha = 0.3) +
+  facet_wrap(~species) + coord_fixed()+ ggtitle(paste(model_type))
+
+ggplot(d, aes(X, Y, colour = log_density)) + geom_point(size = 0.1, alpha = 0.3) +
+  facet_grid(species~start_time) + coord_fixed() + scale_color_viridis_c()+ ggtitle(paste(model_type))
+
+# 'scratch' code was here.
+
+d$species_year <- paste(d$species, d$start_time)
+
+data <- d
+# formula <- log_density ~ after * source + scale(log_depth) # + as.factor(species)
+# formula <- log_density ~ after * source + scale(log_depth) + scale(vect_dist)
+formula <- log_density ~ after + source + scale(log_depth) * as.factor(species_year) + 
+  I((scale(log_depth))^2) * as.factor(species_year)
+
+species_k <- as.integer(as.factor(d$species_year))
+cell_m <- as.integer(as.factor(paste(d$matchobs, d$species_year)))
+
+X_ij <- model.matrix(formula, data)
+head(X_ij)
+mf <- model.frame(formula, data)
+y_i <- model.response(mf, "numeric")
+
+# Calculate the species indexes for the species year random effects:
+x <- dplyr::distinct(select(d, species, species_year))
+species_id_k <- as.integer(as.factor(x$species))
+
+spde <- sdmTMB::make_spde(d$X, d$Y, n_knots = 100)
+sdmTMB::plot_spde(spde)
+# data$sdm_spatial_id <- 1:nrow(data)
+n_s <- nrow(spde$mesh$loc)
+n_k <- length(unique(species_k))
+n_re <- 4
+
+data$sdm_orig_id <- seq(1, nrow(data))
+data$sdm_x <- spde$x
+data$sdm_y <- spde$y
+fake_data <- unique(data.frame(sdm_x = spde$x, sdm_y = spde$y))
+fake_data[["sdm_spatial_id"]] <- seq(1, nrow(fake_data))
+data <- base::merge(data, fake_data,
+  by = c("sdm_x", "sdm_y"),
+  all.x = TRUE, all.y = FALSE
+)
+data <- data[order(data$sdm_orig_id), , drop = FALSE]
+A_sk <- INLA::inla.spde.make.A(spde$mesh,
+  loc = as.matrix(fake_data[, c("sdm_x", "sdm_y"), drop = FALSE])
+)
+
+tmb_data <- list(
+  y_i = y_i,
+  X_ij = X_ij,
+  A_sk = A_sk,
+  A_spatial_index = data$sdm_spatial_id - 1L,
+  spde = spde$spde$param.inla[c("M0", "M1", "M2")],
+  k_i = species_k - 1L,
+  intercept_i = rep(1, nrow(d)),
+  after_i = d$after,
+  source_i = d$source,
+  n_k = n_k,
+  m_i = cell_m - 1L,
+  species_id_k = species_id_k - 1L,
+  n_just_species = max(species_id_k),
+  n_years_per_species = as.numeric(table(species_id_k))
+  # interaction_position = grep("after:source", colnames(X_ij)) - 1
+)
+
+tmb_params <- list(
+  b_j = rep(0, ncol(tmb_data$X_ij)),
+  ln_tau_E = 0,
+  ln_kappa = 0,
+  ln_phi = 0,
+  epsilon_sk = matrix(0, nrow = n_s, ncol = n_k),
+  b_re = matrix(0, nrow = n_k, ncol = n_re),
+  b_re_sp = rep(0, tmb_data$n_just_species),
+  log_gamma = rep(0, n_re - 1 - 1),
+  log_omega = 0,
+  #log_omega = rep(0, max(species_id_k)),
+  b_cell = rep(0, length(unique(tmb_data$m_i))),
+  log_varphi = 0
+)
+
+suppressWarnings(file.remove(
+  "basic_spatial_re_fix_sigma.o",
+  "basic_spatial_re_fix_sigma.so"
+)) # to avoid crashing R
+TMB::compile("basic_spatial_re_fix_sigma.cpp", )
+dyn.load(dynlib("basic_spatial_re_fix_sigma"))
+
+# First just fit the fixed effects:
+tmb_map <- list(
+  ln_tau_E = as.factor(NA),
+  ln_kappa = as.factor(NA),
+  epsilon_sk = factor(rep(NA, length(tmb_params$epsilon_sk))),
+  b_re = factor(matrix(NA, nrow = n_k, ncol = n_re)),
+  # b_re_sp = factor(rep(NA, length(tmb_params$b_re_sp))),
+  log_gamma = factor(rep(NA, length(tmb_params$log_gamma))),
+  log_omega = factor(rep(NA, length(tmb_params$log_omega))),
+  log_varphi = as.factor(NA),
+  b_cell = factor(rep(NA, length(tmb_params$b_cell)))
+)
+
+tmb_obj <- TMB::MakeADFun(
+  data = tmb_data, parameters = tmb_params, map = tmb_map,
+  random = NULL, DLL = "basic_spatial_re_fix_sigma"
+)
+
+tmb_opt <- stats::nlminb(
+  start = tmb_obj$par, objective = tmb_obj$fn, gradient = tmb_obj$gr,
+  control = list(eval.max = 1e4, iter.max = 1e4)
+)
+
+# ML:
+# tmb_random <- c("epsilon_sk", "b_re", "b_cell")
+# REML:
+tmb_random <- c("epsilon_sk", "b_re", "b_cell", "b_re_sp", "b_j")
+
+# Initialize the fixed effects from the first stage:
+set_par_value <- function(opt, par) {
+  as.numeric(opt$par[par == names(opt$par)])
+}
+tmb_params$b_j <- set_par_value(tmb_opt, "b_j")
+tmb_params$ln_phi <- set_par_value(tmb_opt, "ln_phi")
+tmb_params$b_re_sp <- set_par_value(tmb_opt, "b_re_sp")
+
+tmb_obj <- TMB::MakeADFun(
+  data = tmb_data, parameters = tmb_params,
+  random = tmb_random, DLL = "basic_spatial_re_fix_sigma"
+)
+tictoc::tic()
+tmb_opt <- stats::nlminb(
+  start = tmb_obj$par, objective = tmb_obj$fn, gradient = tmb_obj$gr,
+  control = list(eval.max = 1e4, iter.max = 1e4)
+)
+sdr <- TMB::sdreport(tmb_obj)
+tictoc::toc()
+sdr
+
+s <- summary(sdr)
+
+mutate(as.data.frame(s[row.names(s) == "b_j", ]), coefficient = colnames(X_ij)) %>%
+  select(coefficient, Estimate, `Std. Error`)
+
+s[grep("ln|log", row.names(s)), ]
+s[grep("sigma", row.names(s)), , drop = FALSE]
+
+r <- tmb_obj$report()
+r$range
+
+co <- as.data.frame(s[row.names(s) == "b_baci_interaction", ])
+co$species_year <- as.character(unique(as.factor(d$species_year)))
+co <- co %>%
+  mutate(just_species = gsub(" [0-9]+$", "", species_year)) %>%
+  group_by(just_species) %>%
+  mutate(mean_baci_int = mean(Estimate)) %>%
+  ungroup() %>%
+  arrange(-mean_baci_int, -Estimate) %>%
+  mutate(myorder = seq_len(n()))
+
+ggplot(co, aes(forcats::fct_reorder(species_year, myorder), Estimate,
+  colour = just_species,
+  ymin = Estimate - 2 * `Std. Error`, ymax = Estimate + 2 * `Std. Error`
+)) +
+  geom_pointrange() + coord_flip() + xlab("") + ggtitle(paste(model_type))
+
+meta <- s[grep("b_re_sp", row.names(s)), ]
+meta <- as.data.frame(meta)
+row.names(meta) <- NULL
+meta$just_species <- unique(x$species)
+
+ggplot(meta, aes(forcats::fct_reorder(just_species, -Estimate), Estimate,
+  colour = just_species,
+  ymin = Estimate - 2 * `Std. Error`, ymax = Estimate + 2 * `Std. Error`
+)) +
+  geom_pointrange() + coord_flip() + xlab("") + ggtitle(paste(model_type))
+
+    
