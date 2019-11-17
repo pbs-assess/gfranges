81a00fdae7a6a5673c0b49bdab19970d3fafdbcd
ecophilina <paenglis@lakeheadu.ca>
2019-11-17 14:39:32 -0800

    organize biotic vocc code

--- analysis/VOCC/4-vocc-bioclimatic.R
+++ analysis/VOCC/4-vocc-bioclimatic.R
@@ -2,6 +2,8 @@ getwd()
 setwd(here::here("/analysis/VOCC"))
 
 library("gfranges")
+library("dplyr")
+library("ggplot2")
 
 species <- "Redbanded Rockfish"
 spp <- gsub(" ", "-", gsub("\\/", "-", tolower(species)))
@@ -15,10 +17,10 @@ covs <- "-log-both-fixed-depth"
 #########################
 
 model_ssid <- c(1, 3)
+ssid_string <- paste0(model_ssid, collapse = "n")
 dist_intercept <- 0 # input_cell_size / 2
 input_cell_size <- 2
-scale_fac <- 2
-# scale_fac = 3 means that the raster is reprojected to 3 X original grid (2 km)
+scale_fac <- 5 # means that the raster is reprojected to _ X original grid (2 km)
 
 
 #################
@@ -33,21 +35,25 @@ d_all <- readRDS(paste0("data/", spp, "/all-predictions-",
 #   spp, covs, "-imm-bioclimatic-prior-FALSE.rds")) %>% 
 #   filter(ssid %in% model_ssid)
 
-#d_all <- ad_prediction_list[[2]]
-
+# # for climate independent biotic values...
+covs <- "-trawled-ssid"
+d_all <- readRDS(paste0("data/", spp, "/all-predictions-",
+  spp, covs, "-mat-biomass-prior-FALSE.rds")) %>%
+  filter(ssid %in% model_ssid)
 
 #########################
 ### CHOOSE TIME FRAME
 #########################
 
-unique(d_all$year)
-d <- d_all
-indices <- c(1, 1, 1, 2, 2)
-year_range <- 5
-# # OR
-# d <- d_all %>% filter(year > 2011) %>% filter(year < 2017)
-# indices <- c(1,2)
-# year_range <- 2
+# unique(d_all$year)
+# d <- d_all
+# indices <- c(1, 1, 1, 2, 2)
+# year_range <- 5
+# OR
+d <- d_all %>% filter(year > 2009) %>% filter(year < 2015)
+unique(d$year)
+indices <- c(1,2)
+year_range <- 2
 
 # end_time <- start_time + 2
 skip_time <- NULL
@@ -129,7 +135,7 @@ gf0t <- filter(gf0, est_exp_1 > lower_density_threshold) %>%
 
 
 grad0 <- plot_vocc(gf0t,
-  # theme = "black",
+  #theme_black = TRUE,
   coast = TRUE,
   vec_aes = NULL,
   #grad_vec_aes = "velocity",
@@ -177,9 +183,11 @@ gf$est_exp_1 <- exp(gf$est_1) * 10000
 
 range(gf$velocity, na.rm = TRUE)
 
-gf <- mutate(gf, velocity = if_else(gradient < 0.001, NA_real_, velocity))
 gf1 <- filter(gf, est_exp_1 > lower_density_threshold) %>% 
   mutate(velocity = velocity/year_range)
+gf1 <- mutate(gf1, velocity = if_else(gradient < 0.001, NA_real_, velocity))
+
+range(gf1$velocity, na.rm = TRUE)
 
 #gf1z <- mutate(gf1, velocity = if_else(velocity == 0, NA_real_, velocity))
 
@@ -212,8 +220,10 @@ grad1
 
 glimpse(d)
 d[d$year == min(d$year), ]$bioclimatic <- (d[d$year == min(d$year), ]$est)
-# d[d$year==max(d$year), ]$bioclimatic <- d[d$year==max(d$year), ]$est - d[d$year==max(d$year), ]$epsilon_st
-d[d$year == max(d$year), ]$bioclimatic <- (d[d$year == max(d$year), ]$est_non_rf - d[d$year == max(d$year), ]$est_rw_i + d[d$year == min(d$year), ]$est_rw_i - d[d$year == max(d$year), ]$epsilon_st)
+d[d$year==max(d$year), ]$bioclimatic <- d[d$year==max(d$year), ]$est - d[d$year==max(d$year), ]$epsilon_st
+
+# # with time-varying depth
+# d[d$year == max(d$year), ]$bioclimatic <- (d[d$year == max(d$year), ]$est_non_rf - d[d$year == max(d$year), ]$est_rw_i + d[d$year == min(d$year), ]$est_rw_i - d[d$year == max(d$year), ]$epsilon_st)
 
 gf2 <- vocc_gradient_calc(d, "bioclimatic",
   scale_fac = scale_fac,
@@ -233,43 +243,19 @@ gf2 <- gf2 %>%
   dplyr::mutate(X = x, Y = y) %>%
   gfplot:::utm2ll(., utm_zone = 9)
 
-gf2$epsilon_1 <- gf$epsilon_1
-gf2$epsilon_2 <- gf$epsilon_2
-gf2$epsilon_st <- gf$epsilon_st
+gf2 <- left_join(gf2, add_vars, by = c("x","y"))
 
-gf2$est_1 <- gf$est_1
-gf2$est_2 <- gf$est_2
-gf2$est_diff <- gf$est_diff
-gf2$est_exp_1 <- exp(gf$est_1) * 10000
+gf2$est_diff <- exp(gf2$est_2) * 10000 - exp(gf2$est_1) * 10000
+gf2$est_exp_1 <- exp(gf2$est_1) * 10000
+#gf2$epsilon_st <- gf$epsilon_st
 
-range(gf$velocity, na.rm = TRUE)
 
 # gf2 <- mutate(gf2, velocity = if_else(gradient < 0.001, NA_real_, velocity))
-gf2 <- filter(gf2, est_exp_1 > lower_density_threshold) %>% 
+gf2t <- filter(gf2, est_exp_1 > lower_density_threshold) %>% 
   mutate(velocity = velocity/year_range)
-
-grad2 <- gfranges::plot_vocc(gf2,
-  # theme = "black",
-  coast = TRUE,
-  vec_aes = NULL,
-  # grad_vec_aes = "velocity",
-  # vec_lwd_range = c(0.5, 0.5),
-  NA_label = "NA",
-  fill_col = "velocity",
-  fill_label = "gradient velocity",
-  raster_alpha = 1,
-  raster_limits = c(-400, 200),
-  na_colour = "black",
-  # white_zero = TRUE,
-  high_fill = "Steel Blue 4",
-  low_fill = "Red 3",
-  axis_lables = FALSE,
-  transform_col = sqrt # no_trans #
-) + labs(
-  title = "bioclimatic",
-  subtitle = paste0(min(d$year), "-", max(d$year))
-)
-grad2
+grad_threshold <- quantile(gf2t$gradient, 0.001)
+gf2t <- mutate(gf2t, velocity = if_else(gradient < grad_threshold, 
+  NA_real_, velocity))
 
 
 ######################################
@@ -294,18 +280,51 @@ gf3 <- gf3 %>%
   dplyr::mutate(X = x, Y = y) %>%
   gfplot:::utm2ll(., utm_zone = 9)
 
-gf3$epsilon_1 <- gf$epsilon_1
-gf3$epsilon_2 <- gf$epsilon_2
-gf3$epsilon_st <- gf$epsilon_st
+gf3 <- left_join(gf3, add_vars, by = c("x","y"))
 
-gf3$est_1 <- gf$est_1
-gf3$est_2 <- gf$est_2
-gf3$est_diff <- gf$est_diff
-gf3$est_exp_1 <- exp(gf$est_1) * 10000
+gf3$est_diff <- exp(gf3$est_2) * 10000 - exp(gf3$est_1) * 10000
+gf3$est_exp_1 <- exp(gf3$est_1) * 10000
 
 # gf3 <- mutate(gf3, velocity = if_else(gradient < 0.001, NA_real_, velocity))
 gf3t <- filter(gf3, est_exp_1 > lower_density_threshold) %>% 
   mutate(velocity = velocity/year_range)
+
+#grad_threshold <- quantile(gf3t$gradient, 0.0001)
+gf3t <- mutate(gf3t, velocity = if_else(gradient < grad_threshold, 
+  NA_real_, velocity))
+
+
+######################################
+### PLOT GRADIENT-BASED VELOCITY RASTER 
+######################################
+
+r2 <- range(gf2t$velocity, na.rm = TRUE) 
+r3 <- range(gf3t$velocity, na.rm = TRUE) 
+raster_min <- min(c(r2, r3))
+raster_max <- max(c(r2, r3))
+
+grad2 <- gfranges::plot_vocc(gf2t,
+  # theme = "black",
+  coast = TRUE,
+  vec_aes = NULL,
+  # grad_vec_aes = "velocity",
+  # vec_lwd_range = c(0.5, 0.5),
+  NA_label = "NA",
+  fill_col = "velocity",
+  fill_label = "gradient velocity",
+  raster_alpha = 1,
+  #raster_limits = c(raster_min, raster_max),
+  na_colour = "black",
+  # white_zero = TRUE,
+  high_fill = "Steel Blue 4",
+  low_fill = "Red 3",
+  axis_lables = FALSE,
+  transform_col = sqrt # no_trans # 
+) + labs(
+  title = "bioclimatic",
+  subtitle = paste0(min(d$year), "-", max(d$year))
+)
+grad2
 
 grad3 <- gfranges::plot_vocc(gf3t,
   # theme = "black",
@@ -317,15 +336,15 @@ grad3 <- gfranges::plot_vocc(gf3t,
   fill_col = "velocity",
   fill_label = "gradient velocity",
   raster_alpha = 1,
-  raster_limits = c(-400, 200),
+  #raster_limits = c(raster_min, raster_max),
   na_colour = "black",
   # white_zero = TRUE,
   high_fill = "Steel Blue 4",
   low_fill = "Red 3",
   axis_lables = FALSE,
-  transform_col = sqrt # no_trans #
+  transform_col =  sqrt # no_trans #
 ) + labs(
-  title = "biotic",
+  title = "biotic  (from climate independent model) ", #
   subtitle = paste0(min(d$year), "-", max(d$year))
 )
 grad3
@@ -336,15 +355,17 @@ grad3
 ######################################
 
 png(
-  file = paste0("figs/", spp, "/biotic-velocities-g-", scale_fac, "-", spp, covs, "-", ssid_string, "-", min(d$year), "-", max(d$year), ".png"),
+  file = paste0("figs/", spp, "/biotic-velocities-g-", scale_fac, 
+    "-", spp, covs, "-", ssid_string, "-", min(d$year), "-", max(d$year), 
+    ".png"),
   res = 600,
   units = "in",
-  width = 14,
-  height = 6.5
+  width = 8.5,
+  height = 8.5
 )
 gridExtra::grid.arrange(
-  grad1, grad2, grad3,
-  nrow = 1
+  grad1, grad0, grad2, grad3,
+  nrow = 2
 )
 dev.off()
 
@@ -355,7 +376,7 @@ dev.off()
 #################################
 #################################
 
-max_dist_all_years <- 50 * year_range
+max_dist_all_years <- 30 * year_range
 # Calculate means for each time period
 all_years <- unique(d$year)
 
@@ -366,14 +387,17 @@ d1 <- d %>%
   filter(year %in% years) %>%
   mutate(bioclimatic = est) %>%
   group_by(X, Y) %>%
-  summarise_all(mean) %>%
-  mutate(est_rw_1 = est_rw_i)
+  summarise_all(mean) #%>%
+  #mutate(est_rw_1 = est_rw_i)
 d2 <- d %>%
   filter(year %in% years2) %>%
   group_by(X, Y) %>%
   summarise_all(mean)
-d2$est_rw_1 <- d1$est_rw_1
-d2 <- mutate(d2, bioclimatic = est_non_rf - est_rw_i + est_rw_1 - epsilon_st)
+#d2$est_rw_1 <- d1$est_rw_1
+d2 <- mutate(d2, bioclimatic = est_non_rf - epsilon_st)
+
+# # For time-varying 
+#d2 <- mutate(d2, bioclimatic = est_non_rf - est_rw_i + est_rw_1 - epsilon_st)
 
 dmn <- rbind(d1, d2)
 unique(dmn$year)
@@ -407,6 +431,14 @@ sym_temp <- make_vector_data(dmn,
   round_fact = 10
 )
 
+saveRDS(sym_temp, file = paste0("data/", spp, "/dvocc-temp-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+sym_temp <- readRDS(paste0("data/", spp, "/dvocc-temp-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
 # add and mutate variables
 df <- sym_temp %>% mutate(velocity = if_else(slope < 0, -distance, distance))
 df <- left_join(df, add_vars, by = c("icell","x","y"))
@@ -428,8 +460,8 @@ dft <- filter(dft, est_exp_1 > lower_density_threshold)
 # OR trim based on species thresholds and then filter as above
 lower_thresholds <- c(3.6) # 50% values
 upper_thresholds <- c(6.2) # 50% values
-lower_thresholds <- c(3.9) # 75% values
-upper_thresholds <- c(5.6) # 75% values
+# lower_thresholds <- c(3.9) # 75% values
+# upper_thresholds <- c(5.6) # 75% values
 df1 <- trim_vector_data(df, variable_names,
   lower_change, upper_change,
   lower_thresholds, upper_thresholds,
@@ -445,13 +477,12 @@ df1t <- mutate(df1t, velocity = if_else(velocity < -max_dist_all_years / year_ra
 
 # Plot trimmed to 95% of biomass
 gvocc1 <- gfranges::plot_vocc(dft,
-  # theme = "black",
   coast = TRUE,
   vec_aes = NULL,
   fill_col = "velocity",
   fill_label = "velocity",
   raster_alpha = 1,
-  raster_limits = c(0, 20),
+  #raster_limits = c(0, 20),
   na_colour = "darkred",
   white_zero = TRUE,
   vec_alpha = 0.35,
@@ -465,13 +496,12 @@ gvocc1
 
 # Plot trimmmed to species thresholds
 gvocc1t <- gfranges::plot_vocc(df1t,
-  # theme = "black",
   coast = TRUE,
   vec_aes = NULL,
   fill_col = "velocity",
   fill_label = "velocity",
   raster_alpha = 1,
-  raster_limits = c(0, 20),
+  #raster_limits = c(0, 20),
   na_colour = "darkred",
   white_zero = TRUE,
   vec_alpha = 0.35,
@@ -511,6 +541,15 @@ do_vec <- make_vector_data(dmn,
   round_fact = 10
 )
 
+saveRDS(do_vec, file = paste0("data/", spp, "/dvocc-do-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+do_vec <- readRDS(paste0("data/", spp, "/dvocc-do-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+
 # add and mutate variables
 do <- do_vec %>% mutate(velocity = if_else(slope < 0, -distance, distance))
 do <- left_join(do, add_vars, by = c("icell","x","y"))
@@ -536,16 +575,15 @@ do1 <- trim_vector_data(do, variable_names,
     cell_size = 2*scale_fac, dist_intercept = 0,
     max_dist = 120, min_dist = 0
   )
-  do1t <- filter(do1, est_exp_1 > lower_density_threshold)
-  do1t <- mutate(do1t, velocity = if_else(slope < 0, -distance, distance))
-  do1t <- mutate(do1t, velocity = if_else(velocity > max_dist_all_years, 
+do1t <- filter(do1, est_exp_1 > lower_density_threshold)
+do1t <- mutate(do1t, velocity = if_else(slope < 0, -distance, distance))
+do1t <- mutate(do1t, velocity = if_else(velocity > max_dist_all_years, 
     max_dist_all_years, velocity) / year_range)
-  do1t <- mutate(do1t, velocity = if_else(velocity < -max_dist_all_years / year_range, 
+do1t <- mutate(do1t, velocity = if_else(velocity < -max_dist_all_years / year_range,
     -max_dist_all_years / year_range, velocity))
   
 # Plots
 gvocc_do <- gfranges::plot_vocc(dot,
-  # theme = "black",
   coast = TRUE,
   vec_aes = NULL,
   fill_col = "velocity",
@@ -558,7 +596,7 @@ gvocc_do <- gfranges::plot_vocc(dot,
   white_zero = TRUE,
   vec_alpha = 0.35,
   axis_lables = FALSE,
-  transform_col = no_trans
+  transform_col = sqrt #no_trans
 ) + ggtitle("DO decrease > 0.25")
 gvocc_do
 
@@ -577,7 +615,7 @@ gvocc_dot <- gfranges::plot_vocc(do1t,
   vec_alpha = 0.35,
   axis_lables = FALSE,
   transform_col = no_trans
-) + ggtitle("DO decrease > 0.25")
+) + ggtitle("DO decrease > 0.25 & exceeds species thresholds")
 gvocc_dot
 
 
@@ -586,7 +624,7 @@ gvocc_dot
 ######################################
 
 variable_names <- c("bioclimatic")
-biomass_change <- c(0.5)
+biomass_change <- c(0.25)
 # min_est <- quantile(d$bioclimatic, 0.01)
 # lower_thresholds <- NA
 # upper_thresholds <- min_est
@@ -608,8 +646,18 @@ bioclim_vec <- make_vector_data(dmn,
   round_fact = 10
 )
 
+saveRDS(bioclim_vec, file = paste0("data/", spp, "/dvocc-bioclim-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+bioclim_vec <- readRDS(paste0("data/", spp, "/dvocc-bioclim-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac,  
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+#max(df2$distance, na.rm = TRUE)
 # add and mutate variables
-df2 <- bioclim_vec %>% mutate(velocity = if_else(slope < 0, -distance, distance))
+df2 <- bioclim_vec %>% mutate(distance = if_else(distance == max(distance, na.rm = TRUE), NA_real_, distance))
+df2 <- df2 %>% mutate(velocity = if_else(slope < 0, -distance, distance))
 df2 <- left_join(df2, add_vars, by = c("icell","x","y"))
 df2$epsilon_diff <- df2$epsilon_2 - df2$epsilon_1
 df2$est_diff <- (df2$est_2) - (df2$est_1)
@@ -619,6 +667,7 @@ df2$est_exp_1 <- exp(df2$est_1) * 10000
 
 
 # filter for 95% of biomass and truncate velocities
+# max_dist_all_years <- 20 * year_range
 df2t <- filter(df2, est_exp_1 > lower_density_threshold)
 df2t <- mutate(df2t, velocity = if_else(velocity > max_dist_all_years, 
   max_dist_all_years, velocity) / year_range)
@@ -649,9 +698,18 @@ biotic_vec <- make_vector_data(dmn,
   round_fact = 10
 )
 
+saveRDS(biotic_vec, file = paste0("data/", spp, "/dvocc-biotic-", spp, 
+  "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+  covs, "-", lower_change, "-", upper_change, ".rds"))
+
+biotic_vec <- readRDS(paste0("data/", spp, "/dvocc-biotic-", spp, 
+   "-", min(d$year), "-", max(d$year), "-", ssid_string, "-x", scale_fac, 
+   covs, "-", lower_change, "-", upper_change, ".rds"))
 
 # add and mutate variables
-df3 <- biotic_vec %>% mutate(velocity = if_else(slope < 0, -distance, distance))
+df3 <- biotic_vec %>% mutate(distance = if_else(distance == max(distance, na.rm = TRUE), NA_real_, distance))
+#max(df3$distance, na.rm = TRUE)
+df3 <- df3 %>% mutate(velocity = if_else(slope < 0, -distance, distance))
 df3 <- left_join(df3, add_vars, by = c("icell","x","y"))
 df3$epsilon_diff <- df3$epsilon_2 - df3$epsilon_1
 df3$est_diff <- (df3$est_2) - (df3$est_1)
@@ -666,53 +724,69 @@ df3t <- mutate(df3t, velocity = if_else(velocity > max_dist_all_years,
 df3t <- mutate(df3t, velocity = if_else(velocity < -max_dist_all_years / year_range, 
   -max_dist_all_years / year_range, velocity))
 
-
 ######################################
 ### PLOT DISTANCE-BASED VELOCITY RASTER 
 ######################################
 
-# df2t <- mutate(df2t, velocity = if_else(velocity != 0, velocity, NA_real_))
+r2 <- range(df2t$velocity, na.rm = TRUE) 
+r3 <- range(df3t$velocity, na.rm = TRUE) 
+raster_min <- min(c(r2, r3))
+raster_max <- max(c(r2, r3))
+
+
 gvocc2 <- gfranges::plot_vocc(df2t,
-  # theme = "black",
+  # theme_black = TRUE,
   coast = TRUE,
   vec_aes = NULL,
+  # vec_aes = "velocity",
+  # vec_col = "black",
+  # vec_lwd_range = c(1,1),
+  # min_vec_plotted = 5, # this is in distance
+  # max_vec_plotted = max_dist_all_years, # this is in distance
   fill_col = "velocity",
   fill_label = "velocity",
   raster_alpha = 1,
-  # raster_limits = c(-50,15),
-  na_colour = "gray",
+  raster_limits = c(raster_min, raster_max),
+  na_colour = "black",
   white_zero = TRUE,
-  # mid_fill = "black",
   high_fill = "Steel Blue 4",
   low_fill = "Red 3",
+  # mid_fill = "black",
   # high_fill = "#66C2A5",
   # low_fill = "#5E4FA2FF",
   vec_alpha = 0.35,
   axis_lables = FALSE,
-  transform_col = no_trans # sqrt # fourth_root_power #
+  transform_col =  no_trans # sqrt # fourth_root_power # 
 ) + labs(
   title = paste0("bioclimatic - projected change in abundance > ", biomass_change * 100, "%"),
   subtitle = paste0(min(d$year), "-", max(d$year))
 )
 gvocc2
 
-
+#df3v <- df3t %>% mutate(distance = if_else(slope < 0, distance, 0))
 gvocc3 <- gfranges::plot_vocc(df3t,
-  # theme = "black",
+  #theme_black = TRUE,
   coast = TRUE,
   vec_aes = NULL,
+  # vec_aes = "velocity",
+  # vec_col = "black",
+  # vec_lwd_range = c(1,1),
+  # min_vec_plotted = 5, # this is in distance
+  # max_vec_plotted = max_dist_all_years, # this is in distance
   fill_col = "velocity",
   fill_label = "velocity",
   raster_alpha = 1,
-  # raster_limits = c(-50,15),
-  na_colour = "gray",
-  # white_zero = TRUE,
+  #raster_limits = c(raster_min, raster_max),
+  na_colour = "black",
+  white_zero = TRUE,
+  high_fill = "Steel Blue 4",
+  low_fill = "Red 3",
   # mid_fill = "black",
-  high_fill = "Steel Blue 4", 
-  low_fill = "Red 3", 
+  # high_fill = "#66C2A5",
+  # low_fill = "#5E4FA2FF",
   vec_alpha = 0.35,
   axis_lables = FALSE,
-  transform_col = no_trans # fourth_root_power #sqrt #
+  transform_col =  no_trans # fourth_root_power # sqrt #
 ) + labs(
   title = paste0("biotic - modelled change in abundance > ", biomass_change * 100, "%"),
   subtitle = paste0(min(d$year), "-", max(d$year))
@@ -725,7 +799,9 @@ gvocc3
 ######################################
 
 png(
-  file = paste0("figs/", spp, "/biotic-velocities-d-", spp, covs, "-", ssid_string, "-", min(d$year), "-", max(d$year), "-symetrical-", biomass_change, ".png"),
+  file = paste0("figs/", spp, "/biotic-velocities-d-", scale_fac, "-", 
+    spp, covs, "-", ssid_string, "-", min(d$year), "-", max(d$year), 
+    "-symetrical-", biomass_change, ".png"),
   res = 600,
   units = "in",
   width = 8.5,
