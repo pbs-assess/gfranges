90cc7a88b49059208740cee4f0f06c59ccbcd90f
ecophilina <paenglis@lakeheadu.ca>
2019-12-31 17:50:18 -0800

    functionize vocc_regression and save plots of model coeficients

--- analysis/VOCC/vocc-regression.R
+++ analysis/VOCC/vocc-regression.R
@@ -6,17 +6,20 @@ setwd(here::here("analysis", "VOCC"))
 compile("vocc_regression.cpp")
 dyn.load(dynlib("vocc_regression"))
 
-d <- readRDS("~/Downloads/multi-spp-biotic-vocc.rds")
+model <- "multi-spp-biotic-vocc"
+model <- "multi-spp-biotic-vocc-mature"
+#model <- "multi-spp-biotic-vocc-immature"
+
+d <- readRDS(paste0("data/", model, ".rds"))
 d <- na.omit(d) %>% as_tibble()
 
-X_ij <- model.matrix(~scale(temp_vel), data = d)
-# X_ij <- model.matrix(~scale(DO_vel), data = d)
+
+vocc_regression <- function(y_i, X_ij) {
+
 hist(X_ij[,2], breaks = 100)
 range(X_ij[,2])
 
-y_i <- d$biotic_vel
-# y_i <- d$biotic_trend
-# y_i <- log(d$biotic_CV)
+
 hist(y_i, breaks = 100)
 range(y_i)
 
@@ -25,8 +28,7 @@ range(y_i)
 #   scale_color_viridis_c()
 # 
 # ggplot(d, aes(x, y, colour = biotic_vel)) + geom_point() +
-#   facet_wrap(~species) +
-#   scale_color_viridis_c()
+#   facet_wrap(~species) #+ scale_color_viridis_c()
 
 # -----------------------------
 
@@ -63,6 +65,7 @@ tmb_data <- list(
   n_k = n_k,
   nu = 7 # Student-t DF
 )
+
 tmb_param <- list(
   b_j = rep(0, ncol(X_ij)),
   log_gamma = rep(-1, ncol(X_ij)),
@@ -93,6 +96,204 @@ ids <- do.call("rbind", replicate(n_coefs, ids, simplify = FALSE))
 ids[["coefficient"]] <- rep(colnames(X_ij), each = n_spp)
 
 b_re <- as.data.frame(s[grep("b_re", row.names(s)), , drop = FALSE])
-b_re <-  bind_cols(ids, b_re)
-b_re
+b_re <- bind_cols(ids, b_re)
 
+model <- list(obj = obj, sdr = sdr, coefs = b_re)
+}
+
+add_colours <- function(coefs, manual_colours = TRUE) {
+
+if (manual_colours) {
+species <- c(
+  "Arrowtooth Flounder", 
+  "Canary Rockfish",
+  "Curlfin Sole",
+  "Darkblotched Rockfish",
+  "Dover Sole",
+  "English Sole",
+  "Flathead Sole",
+  "Greenstriped Rockfish",
+  "Lingcod",
+  "Longspine Thornyhead",
+  "North Pacific Spiny Dogfish",
+  "Pacific Cod",
+  "Pacific Halibut",
+  "Pacific Ocean Perch",
+  "Petrale Sole",
+  "Quillback Rockfish",
+  "Redbanded Rockfish",
+  "Rex Sole",
+  "Sablefish",
+  "Sand Sole",
+  "Sharpchin Rockfish",
+  "Shortspine Thornyhead",
+  "Silvergray Rockfish",
+  "Southern Rock Sole",
+  "Splitnose Rockfish",
+  "Walleye Pollock",
+  "Widow Rockfish",
+  "Yelloweye Rockfish",
+  "Yellowmouth Rockfish", 
+  "Yellowtail Rockfish"
+)
+   
+#   ## To choose specific colours for specific species 
+#   # RColorBrewer::brewer.pal(n = 10, name = 'Spectral')
+#   # RColorBrewer::display.brewer.pal(n = 10, name = 'Spectral')
+#   # gfutilities::rich.colors(n = 20, alpha = 1)
+
+   colours <- c( 
+     "#3288BD", # "Arrowtooth Flounder", 
+     "#9E0142", # "Canary Rockfish",
+     "#3288BD", #"Curlfin Sole",
+     "#9E0142", # "Darkblotched Rockfish",
+     "#3288BD", #"Dover Sole",
+     "#3288BD", #"English Sole",
+     "#3288BD", #"Flathead Sole",
+     "#9E0142", # "Greenstriped Rockfish",
+     "#F46D43", #""Lingcod",
+     "#D53E4F", #"Longspine Thornyhead",
+     "#FDAE61", #"North Pacific Spiny Dogfish",
+     "#ABDDA4", #""Pacific Cod",
+     "#5E4FA2", #"Pacific Halibut",
+     "#9E0142", # "Pacific Ocean Perch",
+     "#3288BD", #"Petrale Sole",
+     "#9E0142", # "Quillback Rockfish",
+     "#9E0142", # "Redbanded Rockfish",
+     "#3288BD", #"Rex Sole",
+     "#ABDDA4", #""Sablefish",
+     "#3288BD", #"Sand Sole",
+     "#9E0142", # "Sharpchin Rockfish",
+     "#D53E4F", #"Shortspine Thornyhead",
+     "#9E0142", # "Silvergray Rockfish",
+     "#3288BD", #"Southern Rock Sole",
+     "#9E0142", # "Splitnose Rockfish",
+     "#ABDDA4", #"Walleye Pollock",
+     "#9E0142", # "Widow Rockfish",
+     "#9E0142", # "Yelloweye Rockfish",
+     "#9E0142", # "Yellowmouth Rockfish", 
+     "#9E0142"# "Yellowtail Rockfish"
+   )
+   
+   colour_key <- as_tibble(cbind(species, colours))
+   colour_key$species <- as.factor(colour_key$species)
+   
+   out <- left_join(coefs, colour_key, by = "species") 
+   missing_colours <- out$species[is.na(out$colours)] 
+
+   if (length(missing_colours)>0) {
+     stop(paste(missing_colours, "need a colour assigned."))
+  }
+
+  } else {
+  N <- length(unique(coefs$species))
+  #species <- unique(coefs$species)
+  species <- levels(coefs$species)
+  colours <- gfutilities::rich.colors(n = N, alpha = 1)
+  colour_key <- as_tibble(cbind(species, colours))
+  colour_key$species <- as.factor(colour_key$species)
+  out <- left_join(coefs, colour_key, by = "species") 
+ }
+  out <- arrange(out, species)
+  out
+}
+  
+plot_coefs  <- function (coloured_coefs) {
+  coloured_coefs <- filter(coloured_coefs, coefficient != "(Intercept)")
+  colour_list <- coloured_coefs$colours #c(unique(b_re$colours))
+  
+  p <- ggplot(coloured_coefs, aes(
+    forcats::fct_reorder(species, -Estimate), 
+    Estimate,
+    colour = species,
+    ymin = Estimate - 2 * `Std. Error`,
+    ymax = Estimate + 2 * `Std. Error`
+  )) +
+    geom_hline(yintercept = 0, colour = "darkgray") +
+    scale_colour_manual(values = colour_list) + 
+    geom_pointrange() + coord_flip() + xlab("") +
+    theme(legend.position = "none")
+  p
+} 
+
+
+### Local biomass velocity from 2007-2018
+
+y <- d$biotic_vel
+x <- model.matrix(~scale(temp_vel), data = d)
+
+bio_temp <- vocc_regression(y, x)
+bio_temp2 <- add_colours(bio_temp$coefs)
+bio_temp3 <- plot_coefs(bio_temp2) 
+bio_temp_plot <- bio_temp3 + ggtitle(paste("Biotic velocity by thermal VOCC"))
+bio_temp_plot
+
+y <- d$biotic_vel
+x <- model.matrix(~scale(DO_vel), data = d)
+
+bio_do <- vocc_regression(y, x)
+bio_do2 <- add_colours(bio_do$coefs)
+bio_do3 <- plot_coefs(bio_do2) 
+bio_do_plot <- bio_do3 + ggtitle(paste("Biotic velocity by DO VOCC"))
+bio_do_plot
+
+### Local biomass trend from 2007-2018
+
+y <- d$biotic_trend
+x <- model.matrix(~scale(temp_vel), data = d)
+
+trend_temp <- vocc_regression(y, x)
+trend_temp2 <- add_colours(trend_temp$coefs)
+trend_temp3 <- plot_coefs(trend_temp2) 
+trend_temp_plot <- trend_temp3 + ggtitle(paste("Biotic trend by thermal VOCC"))
+trend_temp_plot
+
+
+y <- d$biotic_trend
+x <- model.matrix(~scale(DO_vel), data = d)
+
+trend_do <- vocc_regression(y, x)
+trend_do2 <- add_colours(trend_do$coefs)
+trend_do3 <- plot_coefs(trend_do2) 
+trend_do_plot <- trend_do3 + ggtitle(paste("Biotic trend by DO VOCC"))
+trend_do_plot
+
+### Local biomass variability from 2007-2018
+
+y <- d$biotic_CV
+x <- model.matrix(~scale(temp_vel), data = d)
+
+CV_temp <- vocc_regression(y, x)
+CV_temp2 <- add_colours(CV_temp$coefs)
+CV_temp3 <- plot_coefs(CV_temp2) 
+CV_temp_plot <- CV_temp3 + ggtitle(paste("Biotic CV by thermal VOCC"))
+CV_temp_plot 
+
+
+y <- d$biotic_CV
+x <- model.matrix(~scale(DO_vel), data = d)
+
+CV_do <- vocc_regression(y, x)
+CV_do2 <- add_colours(CV_do$coefs)
+CV_do3 <- plot_coefs(CV_do2) 
+CV_do_plot <- CV_do3 + ggtitle(paste("Biotic CV by DO VOCC"))
+CV_do_plot
+
+png(file = paste0("figs/", model, ".png"),
+      res = 600,
+      units = "in",
+      width = 8.5,
+      height = 11
+    )
+    gridExtra::grid.arrange(
+      grobs = c(list(
+          bio_temp_plot,
+          bio_do_plot,
+          trend_temp_plot,
+          trend_do_plot,
+          CV_temp_plot, 
+          CV_do_plot)),
+      nrow = 3,
+      top = grid::textGrob(paste(model))
+    )
+dev.off()
