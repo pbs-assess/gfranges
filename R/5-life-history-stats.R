d18070afcfdd82cf4eef933105945881a050f304
ecophilina <paenglis@lakeheadu.ca>
2020-01-03 00:43:21 -0800

    tweaks to saving of maturity splits

--- R/5-life-history-stats.R
+++ R/5-life-history-stats.R
@@ -39,14 +39,22 @@ species <- c(
   "Widow Rockfish",
   "Yelloweye Rockfish",
   "Yellowmouth Rockfish",
-  "Yellowtail Rockfish"
+  "Yellowtail Rockfish",
+  "Big Skate",
+  "Longnose Skate",
+  "Spotted Ratfish",
+  "Bocaccio",
+  "Redstripe Rockfish",
+  "Rougheye/Blackspotted Rockfish Complex",
+  "Shortraker Rockfish"
 )
 
-# species <- c("Arrowtooth Flounder")
-# species <- c("Pacific Ocean Perch")
+# species <- c("Spotted Ratfish")
+# species <- c("Shortraker Rockfish")
 # spp <- gsub(" ", "-", gsub("\\/", "-", tolower(species)))
 
 life_history <- purrr::map_dfr(species, function(x) {
+  
   spp <- gsub(" ", "-", gsub("\\/", "-", tolower(x)))
   fish <- readRDS(paste0("raw/bio-data-", spp, ""))
   group <- unique(fish$maturity_convention_desc)
@@ -61,13 +69,21 @@ life_history <- purrr::map_dfr(species, function(x) {
   fish <- left_join(fish, depth)
   
   rm(maturity)
-  try({maturity <- readRDS(paste0("data/", spp, 
-    "/maturity-ogive-", spp, "-1n3n4n16.rds"
-  ))})
+  try({
+    maturity <- readRDS(paste0("data/", spp, 
+    "/maturity-ogive-", spp, "-1n3n4n16.rds"))
+  })
   
   if(exists("maturity")) {
-  length_50_mat_m <- maturity$mat_perc$mean$m.mean.p0.5
-  length_50_mat_f <- maturity$mat_perc$mean$f.mean.p0.5
+    
+    if(maturity$year_re) {
+    length_50_mat_m <- maturity$mat_perc$mean$m.mean.p0.5
+    length_50_mat_f <- maturity$mat_perc$mean$f.mean.p0.5
+    } else {
+    length_50_mat_m <- maturity$mat_perc$m.p0.5
+    length_50_mat_f <- maturity$mat_perc$f.p0.5
+    }
+    
   mat_m <- filter(fish, sex == 1) %>% filter(length > length_50_mat_m)
   mat_f <- filter(fish, sex == 2) %>% filter(length > length_50_mat_f)
   
@@ -83,19 +99,32 @@ life_history <- purrr::map_dfr(species, function(x) {
     
   length_50_mat_m <- NA
   length_50_mat_f <- NA
-  large_threshold <- quantile(fish$length, 0.75, na.rm = TRUE)
-  small_threshold <- quantile(fish$length, 0.25, na.rm = TRUE)
+  large_threshold <- quantile(fish$length, 0.90, na.rm = TRUE)
+  small_threshold <- quantile(fish$length, 0.10, na.rm = TRUE)
   large <- filter(fish, length > large_threshold)
-  small <- filter(fish, length < small_threshold)
+  small <- filter(fish, length < 20)#small_threshold)
   }
+  
+  ## FOR EXPLORING SPECIAL CASES
+  # fish_f <- filter(fish, sex == 2)
+  # fish_m <- filter(fish, sex == 1)
+  # mid <- filter(fish, length > 40)#%>% filter(length<40)#small_threshold)
+  # dep <- mean(mid$depth, na.rm =TRUE)
+  # dep
   
   large_depth <- mean(large$depth, na.rm = TRUE)
   small_depth <- mean(small$depth, na.rm = TRUE)
   
+  prop_pos <- NA
+  try({survey_sets <- readRDS(paste0("raw/event-data-", spp, ""))
+  positive_sets <- filter(survey_sets, density_kgpm2 != 0)
+  prop_pos <- round(nrow(positive_sets)/nrow(survey_sets), digits = 3)
+  })
+  
   list(
     species = x, group = group[1],
-    length_50_mat_m = length_50_mat_m,
-    length_50_mat_f = length_50_mat_f,
+    length_50_mat_m = round(length_50_mat_m),
+    length_50_mat_f = round(length_50_mat_f),
     mat_depth = round(large_depth),
     imm_depth = round(small_depth),
     depth_diff = round(small_depth - large_depth),
@@ -105,11 +134,13 @@ life_history <- purrr::map_dfr(species, function(x) {
     max_age = round(quantile(fish$age, 0.999999, na.rm = TRUE)),
     max_length = max(fish$length, na.rm = TRUE),
     long_length = round(quantile(fish$length, 0.9999, na.rm = TRUE)),
-    large_threshold = large_threshold,
-    small_threshold = small_threshold
+    large_threshold = large_threshold[[1]],
+    small_threshold = small_threshold[[1]],
+    prop_pos_sets = prop_pos
   )
 })
 
 View(life_history)
+life_history$group[is.na(life_history$group)] <- "SKATE"
 
 saveRDS(life_history, file = "data/life-history-stats.rds")
--- analysis/VOCC/render-maturity.R
+++ analysis/VOCC/render-maturity.R
@@ -2,38 +2,40 @@ getwd()
 setwd(here::here("/analysis/VOCC"))
 env <- new.env() #parent = baseenv()
 
+# SPECIES with maturity
 list_species <- c(
-  # "Walleye Pollock",
-  # "Pacific Cod",
-  # "Sablefish",
-  # "Lingcod",
+  "Bocaccio",
+  "Redstripe Rockfish",
+  "Rougheye/Blackspotted Rockfish Complex",
+  "Shortraker Rockfish",
+  "Walleye Pollock",
+  "Pacific Cod",
+  "Sablefish",
+  "Lingcod",
   # "Pacific Hake",
-  # "North Pacific Spiny Dogfish",
-  # "Spotted Ratfish",
   # "Pacific Tomcod",
   # "Rosethorn Rockfish",
-  # "Redstripe Rockfish",
-  # "Yellowmouth Rockfish",
+  "Redstripe Rockfish",
+  "Yellowmouth Rockfish",
   # "Harlequin Rockfish"
-   # "Bocaccio", # winter-birthing, overfished
-  # "Canary Rockfish", # schooling, winter-birthing
-  # "Copper Rockfish", # small sample
-  # "Darkblotched Rockfish",
-  # "Greenstriped Rockfish",
-  # "Pacific Ocean Perch", # schooling
-  # "Quillback Rockfish",
-  # "Redbanded Rockfish",
-  # "Rougheye/Blackspotted Rockfish Complex",
-  # "Sharpchin Rockfish",
-  # "Shortbelly Rockfish", # small sample
-  # "Silvergray Rockfish",
-  # "Splitnose Rockfish",
-  # "Widow Rockfish", # schooling
-  # "Yellowtail Rockfish", # schooling
-  # "Yelloweye Rockfish", # summer-birthing, overfished,
-  # "Longspine Thornyhead",
-  # "Shortspine Thornyhead",
-  #"Arrowtooth Flounder",
+  "Bocaccio", # winter-birthing, overfished
+  "Canary Rockfish", # schooling, winter-birthing
+  "Darkblotched Rockfish",
+  "Greenstriped Rockfish",
+  "Pacific Ocean Perch", # schooling
+  "Quillback Rockfish",
+  "Redbanded Rockfish",
+  "Rougheye/Blackspotted Rockfish Complex",
+  "Sharpchin Rockfish",
+  "Shortbelly Rockfish", # small sample
+  "Silvergray Rockfish",
+  "Splitnose Rockfish",
+  "Widow Rockfish", # schooling
+  "Yellowtail Rockfish", # schooling
+  "Yelloweye Rockfish", # summer-birthing, overfished,
+  "Longspine Thornyhead",
+  "Shortspine Thornyhead",
+  "Arrowtooth Flounder",
   "Rex Sole",
   "Petrale Sole",
   "English Sole",
@@ -41,11 +43,22 @@ list_species <- c(
   "Southern Rock Sole",
   "Flathead Sole",
   "Curlfin Sole",
-  "Sand Sole",
-  "Slender Sole",
-  "Pacific Sanddab",
-  "Pacific Halibut"
+  "Sand Sole"
+  # "Slender Sole",
+  # "Pacific Sanddab"
 )
+
+
+# # SPECIES WITHOUT maturity
+# list_species <- c(
+#   "North Pacific Spiny Dogfish",
+#   "Big Skate",
+#   "Longnose Skate",
+#   "Spotted Ratfish",
+#   "Pacific Halibut"
+# )
+
+
 
 list_regions <- c("All synoptic surveys")
 # list_regions <- c(
@@ -62,7 +75,8 @@ for (r_h in seq_along(list_regions)) {
         rmarkdown::render("1-split-biomass-by-maturity.Rmd",
           params = list(
             species = list_species[spp_i],
-            region = list_regions[r_h]
+            region = list_regions[r_h],
+            split = TRUE
           ),
           output_file = paste0("html/maturity/maturity-", spp, ".html"),
           envir = env
@@ -71,50 +85,7 @@ for (r_h in seq_along(list_regions)) {
     }
 }
 
-
-# # Species run so far...
-# species <- "Arrowtooth Flounder"
-# species <- "Petrale Sole"
-# species <- "English Sole"
-# species <- "Dover Sole" # maturity data error for WCHG
-# species <- "Southern Rock Sole"
-# species <- "Flathead Sole"
-
-
-
-# species <- "Silvergray Rockfish"
-# species <- "Quillback Rockfish" # summer-birthing
-# species <- "Yelloweye Rockfish" # summer-birthing, overfished, 
-# species <- "Bocaccio" # winter-birthing, overfished
-# species <- "Sharpchin Rockfish"
-# species <- "Splitnose Rockfish"
-
-# species <- "Rougheye/Blackspotted Rockfish Complex"
-# species <- "Redbanded Rockfish"
-# species <- "Greenstriped Rockfish"
 # # species <- "Copper Rockfish" # small sample
-# species <- "Darkblotched Rockfish"
 # # species <- "Shortbelly Rockfish" # small sample
-
-# species <- "Pacific Ocean Perch" # schooling
-# species <- "Widow Rockfish" # schooling
-# species <- "Yellowtail Rockfish" # schooling
-# species <- "Canary Rockfish" # schooling, winter-birthing
-# species <- "Shortraker Rockfish"
-
-# species <- "Longspine Thornyhead"
-# species <- "Shortspine Thornyhead"
-
-
-# species <- "Walleye Pollock"
-# species <- "Pacific Cod"
-# species <- "Sablefish"
-# species <- "Lingcod"
-# species <- "Pacific Hake"
-
-# species <- "North Pacific Spiny Dogfish" # note: using all data for maturity thresholds
-# species <- "Longnose Skate"
-# species <- "Big Skate"
-# species <- "Spotted Ratfish"
 # # species <- "Sandpaper Skate" # small sample
 # # species <- "Brown Cat Shark" # small sample
