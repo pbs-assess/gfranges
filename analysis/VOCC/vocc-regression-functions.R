fb52462f5504263fe8577752678e12ad42d17d8b
ecophilina <paenglis@lakeheadu.ca>
2020-02-06 14:29:09 -0800

    add na.rm = T to collapse_outliers quantile choice

--- analysis/VOCC/vocc-regression-functions.R
+++ analysis/VOCC/vocc-regression-functions.R
@@ -1,6 +1,6 @@
 collapse_outliers <- function(.x, outliers) {
-  .x_max <- quantile(.x, outliers[2])
-  .x_min <- quantile(.x, outliers[1])
+  .x_max <- quantile(.x, outliers[2], na.rm = T)
+  .x_min <- quantile(.x, outliers[1], na.rm = T)
   .x[.x > .x_max] <- .x_max
   .x[.x < .x_min] <- .x_min
   .x
