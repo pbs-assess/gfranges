2430162065226e3876e0257e695a82c9d8d30d7a
ecophilina <paenglis@lakeheadu.ca>
2020-02-06 17:25:48 -0800

    pool velocities before biomass filtering

--- analysis/VOCC/pool-biotic-velocities.R
+++ analysis/VOCC/pool-biotic-velocities.R
@@ -3,15 +3,6 @@ setwd(here::here("/analysis/VOCC/data"))
 library("dplyr")
 
 # covs <- "-fixed"
-# mydir = paste0( "biotic_test1")
-mydir <- paste0("_new_biotic/mat")
-mydir = paste0( "_new_biotic/imm")
-mydir = paste0( "_scrambled1")
-mydir = paste0( "_scrambled2")
-mydir = paste0( "_scrambled3")
-mydir = paste0( "_scrambled4")
-mydir = paste0( "_scrambled5")
-mydir = paste0( "_all_temp")
 mydir = paste0( "_new_null")
 mydir = paste0( "_do_null")
 
@@ -22,21 +13,6 @@ biotic <- do.call(rbind, lapply(myfiles, read.csv)) %>% select(-X, -start_year)
 glimpse(biotic)
 
 #####################
-saveRDS(biotic, file = paste0(
-  "multi-spp-biotic-vocc-mature-with-fished.rds"
-))
-
-saveRDS(biotic, file = paste0(
-  "multi-spp-biotic-vocc-immature-with-fished.rds"
-))
-
-saveRDS(biotic, file = paste0(
-  "scrambled5-mature-with-fished.rds"
-))
-
-saveRDS(biotic, file = paste0(
-  "all-temp-mature-with-fished.rds"
-))
 
 saveRDS(biotic, file = paste0(
   "mature-all-temp-untrimmed.rds"
