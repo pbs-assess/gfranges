2ae05f81a686a272d31421c8e98e757420a465b0
ecophilina <paenglis@lakeheadu.ca>
2020-02-24 10:29:07 -0800

    cleanup plot code

--- analysis/VOCC/7-chopsticks.R
+++ analysis/VOCC/7-chopsticks.R
@@ -3,56 +3,51 @@
 #### CHOPSTICK PLOTS
 ##############################
 ##############################
+library(TMB)
+library(dplyr)
+library(ggplot2)
+library(gfranges)
+
+setwd(here::here("analysis", "VOCC"))
+# compile("vocc_regression.cpp")
+# dyn.load(dynlib("vocc_regression"))
+source("vocc-regression-functions.R")
+
+##############################
+#### LOAD MODELS ####
+
+#### ONE JUST BUILT
 model <- new_model
 
-model <- readRDS("data/trend_01-25-mature-95-all-temp-all-main-2.rds")
-data_type <- "mature-95-all-temp"
 
-
-model <- readRDS("data/trend_01-25-mature-80-all-temp-all-main-2.rds")
-data_type <- "mature-80-all-temp"
-
-model <- readRDS("data/trend_01-25-mature-80-all-temp-all-main-sim-2.rds")
-data_type <- "mature-80-all-temp-sim"
-
-
-model <- readRDS("data/trend_01-25-mature-90-all-temp-all-main.rds") # 90
-data_type <- "mature-90-all-temp"
-
-model <- readRDS("data/trend_01-25-mature-90-all-temp-all-main-sim-2.rds")
-data_type <- "mature-90-all-temp-sim"
-
-
-model <- readRDS("data/trend-mature-95-all-do-02-20-trend-with-do-sim-1.rds")
-data_type <- "mature-95-with-do-sim"
-
-
-model <- readRDS("data/trend-mature-95-all-do-02-20-trend-with-do-1.rds")
-data_type <- "mature-95-with-do"
-
-
-#### INTERACTIONS WIHT TEMP TREND
 title_all <- "Interation plots "
-y_label <- "Predicted biomass trend"
 
+
+### SUBTITLE WITH DATA TYPE
+data_type <- "mature-95-with-do"
+# data_type <- "mature-95-with-do-sim"
+
+# data_type <- "mature-80-all-temp"
+# data_type <- "mature-80-all-temp-sim"
+# data_type <- "mature-90-all-temp"
+# data_type <- "mature-90-all-temp-sim"
+# data_type <- "mature-95-all-temp"
+# data_type <- "mature-95-all-temp-sim"
+
+
+### Y LABEL
+y_label <- "Predicted biomass trend"
+# y_label <- "Predicted biomass vel"
+## y_label <- "Predicted biomass dist-velocity" # didn't converge
+
+
+#### INTERACTIONS WITH CLIMATE TRENDS
 plot_fuzzy_chopsticks(model,
   x_variable = "temp_trend_scaled", type = "temp",
   y_label = y_label
 ) + #ylim(-1.5, 1) + # 
   #facet_wrap(vars(species), scales="free_y") +
   ggtitle(paste(title_all, "(", data_type, ")"))
-
-# plot_fuzzy_chopsticks(model,
-#   x_variable = "temp_trend_scaled", type = "biomass",
-#   y_label = y_label
-# ) + # facet_wrap(vars(species), scales="free_y") +
-#   ggtitle(paste(title_all, "(", data_type, ")"))
-
-# plot_fuzzy_chopsticks(model,
-#   x_variable = "temp_trend_scaled", type = "temp_grad",
-#   y_label = y_label
-# ) + facet_wrap(vars(species), scales = "free_y") +
-#   ggtitle(paste(title_all, "(", data_type, ")"))
 
 plot_fuzzy_chopsticks(model,
   x_variable = "DO_trend_scaled", type = "do",
@@ -63,10 +58,7 @@ plot_fuzzy_chopsticks(model,
 
 
 
-
-title_all <- "Interation plots "
-y_label <- "Predicted biomass vel"
-
+#### INTERACTIONS WITH CLIMATE VELOCITIES
 plot_fuzzy_chopsticks(model,
   x_variable = "squashed_temp_vel_scaled", type = "temp",
   y_label = y_label
@@ -81,14 +73,7 @@ plot_fuzzy_chopsticks(model,
   ggtitle(paste(title_all, "(", data_type, ")"))
 
 
-plot_fuzzy_chopsticks(model,
-  x_variable = "squashed_temp_vel_scaled", type = "biomass",
-  y_label = y_label
-) + # facet_wrap(vars(species), scales="free_y") +
-  ggtitle(paste(title_all, "(", data_type, ")"))
 
-
-y_label <- "Predicted biomass dist-velocity"
 plot_fuzzy_chopsticks(model,
   x_variable = "temp_dvocc_scaled", type = "temp",
   y_label = y_label
@@ -96,41 +81,21 @@ plot_fuzzy_chopsticks(model,
   # facet_wrap(vars(species), scales="free_y") +
   ggtitle(paste(title_all, "(", data_type, ")"))
 
-
-y_label <- "Predicted biomass dist-velocity"
 plot_fuzzy_chopsticks(model,
-  x_variable = "DO_dvocc_scaled", type = "temp",
+  x_variable = "DO_dvocc_scaled", type = "do",
   y_label = y_label
 ) + #ylim(-1.5, 1) + # 
   # facet_wrap(vars(species), scales="free_y") +
   ggtitle(paste(title_all, "(", data_type, ")"))
 
-
-
-#### INTERACTIONS WITH DO TREND
+### for bi-variable dist-search
 # plot_fuzzy_chopsticks(model,
-#   x_variable = "DO_trend_scaled", type = "do",
+#   x_variable = "DO_dvocc_scaled", type = "temp",
 #   y_label = y_label
-# ) +
-# plot_fuzzy_chopsticks(model,
-#   x_variable = "mean_temp_scaled", type = "mean_temp",
-#   y_label = y_label
-# ) + ggtitle(paste(title_all, "(", data_type, ")"))
-plot_fuzzy_chopsticks(model,
-  x_variable = "mean_DO_scaled", type = "mean_do",
-  y_label = y_label
-) + ggtitle(paste(title_all, "(", data_type, ")"))
+# ) + #ylim(-1.5, 1) + # 
+#   # facet_wrap(vars(species), scales="free_y") +
+#   ggtitle(paste(title_all, "(", data_type, ")"))
 
-##############################
-#### LOAD VELOCITY MODELS ####
-# model <- readRDS("data/trend_by_vel_01-16-multi-spp-biotic-vocc-mature-chopsticks3.rds")
-# title_all <- "Interation plots "
-# y_label <-"Predicted biomass velocity"
 
-#### INTERACTIONS WITH VELOCITY COVARIATES
-# plot_fuzzy_chopsticks(model,
-#   x_variable = "squashed_temp_vel_scaled", type = "temp",
-#   y_label = y_label
-# ) + #ylim(-1,1) + #facet_wrap(vars(species), scales="free_y") +
-# ggtitle(paste(title_all, "(", data_type, ")"))
+
 # ggsave("figs/interation-plot-trend-by-temp-vel.png", width = 10, height = 10, dpi = 300)
