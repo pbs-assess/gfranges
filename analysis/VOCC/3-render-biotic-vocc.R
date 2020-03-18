bcc570682e95061c5d08582a4d5f8a124e2107a4
ecophilina <paenglis@lakeheadu.ca>
2020-03-18 11:51:02 -0700

    render immature voccs

--- analysis/VOCC/3-render-biotic-vocc.R
+++ analysis/VOCC/3-render-biotic-vocc.R
@@ -59,7 +59,8 @@ list_regions <- c(
   #   "All synoptic surveys"
 )
 
-
+# age <- "mature"
+if age == "mature") {
 for (r_h in seq_along(list_regions)) {
   for (spp_i in seq_along(list_species)) {
     spp <- gsub(" ", "-", gsub("\\/", "-", tolower(list_species[spp_i])))
@@ -83,9 +84,40 @@ for (r_h in seq_along(list_regions)) {
     })
   }
 }
+}
+
+# age <- "immature"
+if age == "immature") {
+for (r_h in seq_along(list_regions)) {
+  for (spp_i in seq_along(list_species)) {
+    spp <- gsub(" ", "-", gsub("\\/", "-", tolower(list_species[spp_i])))
+    covs <- "-tv-depth-only" # string describing model
+    reg <- gsub(" ", "-", gsub("\\/", "-", tolower(list_regions[r_h])))
+    try({
+      rmarkdown::render("3-vocc-dist.Rmd",
+        params = list(
+          species = list_species[spp_i],
+          immature = TRUE,
+          region = list_regions[r_h],
+          covs = covs
+        ),
+        output_file = paste0(
+          "html/VOCC-plots/vocc-w-do-", spp,
+          "-imm",
+          covs, "-", reg, "-dvocc.html"
+        ),
+        envir = env
+      )
+    })
+  }
+}
+}
+
+
 
 ## ALL YEARS WITH TEMP ONLY
-
+# age <- "mature"
+if age == "mature") {
 for (r_h in seq_along(list_regions)) {
   for (spp_i in seq_along(list_species)) {
     spp <- gsub(" ", "-", gsub("\\/", "-", tolower(list_species[spp_i])))
@@ -107,5 +139,33 @@ for (r_h in seq_along(list_regions)) {
         envir = env
       )
     })
+  }
+}
+}
+
+# age <- "immature"
+if age == "immature") {
+  for (r_h in seq_along(list_regions)) {
+    for (spp_i in seq_along(list_species)) {
+      spp <- gsub(" ", "-", gsub("\\/", "-", tolower(list_species[spp_i])))
+      covs <- "-tv-depth-only" # string describing model
+      reg <- gsub(" ", "-", gsub("\\/", "-", tolower(list_regions[r_h])))
+      try({
+        rmarkdown::render("3-vocc-temp.Rmd",
+          params = list(
+            species = list_species[spp_i],
+            immature = TRUE,
+            region = list_regions[r_h],
+            covs = covs
+          ),
+          output_file = paste0(
+            "html/biotic-vocc/temp-only-", spp,
+            "-imm",
+            covs, "-", reg, "-untrimmed.html"
+          ),
+          envir = env
+        )
+      })
+    }
   }
 }
