48ee197bab79ca74c1f98bb0a56afbdd2b15fc8b
ecophilina <paenglis@lakeheadu.ca>
2020-03-18 11:04:56 -0700

    sopo stats and maps

--- analysis/SOPO/trawl-stats.R
+++ analysis/SOPO/trawl-stats.R
@@ -3,6 +3,7 @@ setwd(here::here("/analysis/SOPO"))
 library("dplyr")
 library("ggplot2")
 library("grid")
+library("gfranges")
 options(scipen = 999)
 
 
@@ -14,6 +15,17 @@ data <- rbind(data, complex) %>% filter(year==2019)
 
 all_catch <- readRDS("data/all_trawl_catch.rds")
 all_catch <- rbind(all_catch, complex)
+
+shortb <- data %>% filter(species_common_name == "shortbelly rockfish")
+sum(shortb$catch_weight)
+all_shortb <- all_catch %>% filter(species_common_name == "shortbelly rockfish") %>% View()
+sum(shortb$catch_weight)/sum(all_shortb$catch_weight)
+
+chili <- data %>% filter(species_common_name == "chilipepper")
+sum(chili$catch_weight)
+all_chili <- all_catch %>% filter(species_common_name == "chilipepper") %>% View()
+sum(chili$catch_weight)/sum(all_chili$catch_weight)
+
 
 inverts <- readRDS("data/all_trawl_invert.rds")
 
@@ -65,26 +77,32 @@ View(raw_dat)
 length(unique(raw_dat$fishing_event_id))
 
 hbll <- readRDS("data/all_hbll_in.rds") 
-View(hbll1)
 n_grid <- load("~/github/dfo/gfplot/data/hbll_n_grid.rda")
 
-hbll1 <- filter(hbll, species_code == 394 & year == 2019)  
-
+hbll1 <- filter(hbll, species_code == 394 & survey_abbrev == "HBLL INS N")  
+View(hbll1)
+all_years <- unique(hbll1$year)
 # hbll1 <- filter(hbll, species_common_name == "yelloweye rockfish" & year == 2019)  
 
 hbll_tidy <- #gfplot::
   tidy_survey_sets(hbll1, survey = "HBLL INS N",
-  years = 2019, density_column = "density_ppkm2") %>% mutate(akima_depth = depth)
-View(hbll_tidy)
+  years = all_years, 
+    density_column = "density_ppkm2") %>% mutate(akima_depth = depth)
+# View(hbll_tidy)
 
 hbll_tidy <- gfplot:::scale_survey_predictors(hbll_tidy)
+tidy_grid <- hbll_tidy
+tidy_grid$year <- 2019
 
-pg <- make_prediction_grid(hbll_tidy, survey = NULL, draw_boundary = FALSE
+pg <- make_prediction_grid(tidy_grid, survey = NULL, draw_boundary = FALSE
 )$grid 
+pg <- as.data.frame(pg)
+pg <- pg %>% mutate(depth = akima_depth, combined = depth)
 
-pg <- mutate(pg, combined = depth)
+hbll_2019 <- hbll_tidy %>% filter(year == 2019)
 
-plot_survey_sets(pg, hbll_tidy, fill_column = "combined",
+##### HBLL MAP
+plot_survey_sets(pg, hbll_2019, fill_column = "combined",
   fill_scale =
     ggplot2::scale_fill_viridis_c( option = "D", direction = -1), #trans= "sqrt",
   colour_scale =
@@ -92,11 +110,11 @@ plot_survey_sets(pg, hbll_tidy, fill_column = "combined",
   pos_pt_col = "black", #"#FFFFFF60",
   bin_pt_col = "black",#"#FFFFFF40",
   pos_pt_fill = "black",#"#FFFFFF05",
-  pt_size_range = c(0.8, 0.8),
+  pt_size_range = c(1, 1),
   show_legend = T,
   extrapolate_depth = T,
   extrapolation_buffer = 0,
-  show_model_predictions = T,
+  show_model_predictions = F,
   show_raw_data = TRUE,
   utm_zone = 9,
   fill_label = "Depth (m)",
@@ -108,10 +126,10 @@ plot_survey_sets(pg, hbll_tidy, fill_column = "combined",
   ylim = NULL,
   x_buffer = c(-5, 5),
   y_buffer = c(-5, 5),
-  north_symbol = T,
-  north_symbol_coord = c(550, 6000),
-  north_symbol_length = 30,
-  cell_size = 0.6, circles = T)
+  north_symbol = F,
+  north_symbol_coord = c(810, 5630),
+  north_symbol_length = 10,
+  cell_size = 2, circles = T)
 
 
 
