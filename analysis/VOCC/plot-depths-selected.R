19e9925810307f15f50077a8aac4397f001a1e11
ecophilina <paenglis@lakeheadu.ca>
2021-05-17 13:43:26 -0700

    remove tags; add units on depth

--- analysis/VOCC/plot-depths-selected.R
+++ analysis/VOCC/plot-depths-selected.R
@@ -32,7 +32,7 @@ plot_tv_depth <- function(
   d <- time_varying_density(adult_biomass, predictor = "depth")
   d$x <- exp(d$x)
   p <- plot_mountains(d,
-    variable_label = "Depth", 
+    variable_label = "Depth (m)", 
     peaklines = F,
     mountains_only = F,
     xlimits = c(0, 605)) +
@@ -41,14 +41,14 @@ plot_tv_depth <- function(
 }
 
 
-(p1 <- plot_tv_depth("Greenstriped Rockfish")+ 
-    labs(tag = "a."))
-(p2 <- plot_tv_depth("Lingcod")+ 
-    labs(tag = "b."))
-(p3 <- plot_tv_depth("North Pacific Spiny Dogfish")+ 
-    labs(tag = "c."))
-(p4 <- plot_tv_depth("Spotted Ratfish")+ 
-    labs(tag = "d."))
+(p1 <- plot_tv_depth("Greenstriped Rockfish") #+ labs(tag = "a.")
+   )
+(p2 <- plot_tv_depth("Lingcod")#+ labs(tag = "b.")
+    )
+(p3 <- plot_tv_depth("North Pacific Spiny Dogfish") #+ labs(tag = "c.")
+  )
+(p4 <- plot_tv_depth("Spotted Ratfish") #+ labs(tag = "d.")
+  )
 
 # make y axis label
 p0 <- ggplot(data.frame(l = p1$labels$y, x = 1, y = 1)) +
@@ -61,13 +61,14 @@ p1$labels$y <- p2$labels$y <- p3$labels$y <- p4$labels$y <- " "
 p1$labels$x <- p2$labels$x <- " "
 
 # p0 + p1 + p2 + p3 + p4 + 
-p0 + (p1 | p2) / (p3 | p4) +
+(tv4 <- p0 + (p1 | p2) / (p3 | p4) +
   plot_layout(
-    widths = c(1, 40),
+    widths = c(1.5, 40),
     # design = layout,
     # ncol = 3,
     guides = 'collect')&
   theme(plot.tag.position = c(.03, .965),
-    plot.tag = element_text(size = 12, hjust = 0, vjust = 0))
+    plot.tag = element_text(size = 12, hjust = 0, vjust = 0)))
 
-ggsave("time-vary-depths.png")  
+saveRDS(tv4, "time-vary-depths.rds")
+ggsave("time-vary-depths.png", width = 7.5, height = 5)  
